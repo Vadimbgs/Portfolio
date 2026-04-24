bfbb26f1b2f183b02f3405c623bd425c373f8ab5
Incognito <163393531+Vadimbgs@users.noreply.github.com>
2026-04-24 14:49:17 +0300

    Add files via upload

---
+++ gronberg_9/js/script.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener('click', documentActions)
+
+function documentActions(e) {
+	const targetElemnt = e.target
+	if (targetElemnt.closest('.menu__icon')) {
+		document.documentElement.classList.toggle('menu-open')
+	}
+}
